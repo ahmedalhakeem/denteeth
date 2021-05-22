68ed2010ce252e0dd1c0b1aeca42a9c0db31ac02
ahmedalhakeem <ahmkam2011@gmail.com>
2021-05-22 23:51:30 +0300

    update and style

--- dentist/static/dentist/next_appointment.js
+++ dentist/static/dentist/next_appointment.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(){
+
+    //
+    
+    //
+
+
+
     document.addEventListener('click', event=>{
         const element = event.target;
         if(element.className === 'edit'){
             //change the title to save
             const id = element.id;
             console.log(id);
-            window.open(`/modify_appointment/${id}`, "_blank", "top=100,left=200,height=400, width=900")   
+            window.open(`/change_appointment/${id}`, "_blank", "top=100,left=200,height=400, width=900")   
             element.innerHTML = 'save';
             const parent = element.parentElement;
             const grandparent = parent.parentElement;
@@ -15,17 +22,97 @@ document.addEventListener('DOMContentLoaded', function(){
 
         }
     })
-    
+    //archive appointment//
     document.addEventListener('click', event=>{
-        const item = event.target;
-        if(item.className === 'remove'){
-            const parentitem = item.parentElement;
-            const granditem = parentitem.parentElement;
-            console.log(granditem);
+        const atom = event.target;
+        if(atom.id === 'archive'){
+            // console.log(patient_name)
+            const parent = atom.parentElement;
+            const grandparent = parent.parentElement;
+            const notes = document.querySelector('#notes');
+            const patient_name = document.querySelector('#patient_name');
+            // const treatment = document.querySelector('#treatment')
+            const paid = document.querySelector('#paid_amount')
+            const name = grandparent.children[0].innerHTML;
+            const treatment_type = document.querySelector('#treatment_type');
+            treatment_type.value = grandparent.children[1].innerHTML;
+            notes.value = grandparent.children[3].innerHTML;
+            // paid.value = grandparent.children[4].innerHTML;
+            patient_name.value = name;
+            // treatment.value = grandparent.children[1].innerHTML;
+           
             
-            granditem.remove();
-        }
+            //console.log('dddd');
+            document.querySelector('#new_appointment').style.display = 'block';
+            document.querySelector('#resechedule-form').style.display = 'none';
+            const id = parent.children[0].id;
+            patient_name.dataset.id = id;
+            console.log(id);
+            // const data ={'name': name, "treatment": treatment, 'notes': notes, }
 
-        //
-    });
+            
+
+
+            
+
+
+
+        }
+        
+    })
+   
 });
+function show_schedule_form(){
+    document.querySelector('#new_appointment').style.display = 'none';
+    document.querySelector('#resechedule-form').style.display = 'block';
+    
+}
+
+function update_schedule(){
+    // 
+    const patient_name = document.querySelector('#patient_name');
+    // const treatment = document.querySelector('#medlist').value;
+    // console.log(treatment)
+    const treatment = document.querySelector('#treatment_type').value;
+    const paid_amount = document.querySelector('#paid_amount').value;
+    const date = document.querySelector('#datepicker').value;
+    const notes = document.querySelector('#notes').value;
+    const id = patient_name.dataset.id;
+
+    const data = {'id': id,'name': patient_name, 'treatment': treatment, 'paid_amount': paid_amount, 'date': date, 'notes': notes}
+    console.log(data);
+
+    fetch('update_schedule',{
+        method: 'POST',
+        headers: {
+            'Content-TYPE' : 'application/json',
+        },
+        body: JSON.stringify(data), 
+
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    })
+}
+function archived(){
+    const patient_name = document.querySelector('#patient_name');
+    const paid_amount = document.querySelector('#paid_amount').value;
+    console.log(paid_amount);
+    const id = patient_name.dataset.id;
+    console.log(id); 
+    const archive_data = {'id': id, 'paid_amount': paid_amount}
+    fetch('archived', {
+        method: 'POST',
+        headers: {
+            'Content-TYPE' : 'application/json',
+        },
+        body : JSON.stringify(archive_data),
+        
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    })
+    window.location.reload();
+}
--- dentist/views.py
+++ dentist/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.contrib.auth.decorators import login_required
@@ -6,6 +7,7 @@ from .forms import *
 from .models import *
 from datetime import datetime
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
@@ -52,8 +54,9 @@ def add_patient(request):
             gender = patient.cleaned_data['gender']
             contact = patient.cleaned_data['contact']
             
-            #save it in the database
+            #Create a new patient 
             new_patient = Patients(name=name, age=age, gender=gender, contact_number=contact)
+            # check if this newly created patient is already existed in the DB.
             all_patients = Patients.objects.all()
             list = []
             for e in all_patients:
@@ -76,7 +79,7 @@ def add_patient(request):
             "patient_form" : patient_form
         })
 # Display all patient names
-def patients(request):
+def all_patients(request):
     patients = Patients.objects.all()
     return render(request, 'dentist/patients.html',{
         "patients": patients
@@ -86,91 +89,111 @@ def patients(request):
 def patient(request, patient_id):
     patient = Patients.objects.get(pk=patient_id)
     #list all treatments for a particular patient
-    treatments = Treatment.objects.filter(p_name=patient)
-    print(treatments)
-    
+    # treatments = Treatment.objects.filter(p_name=patient) //remove
     return render(request, 'dentist/patient.html',{
-        "patient": patient,
-        "treatments": treatments
+        "patient": patient
     })
-# makes a new  appointment
+
+# make a new  appointment
 def appointment(request):
     if request.method == "POST":
 
         app_form = Appointment(request.POST or None)
         if app_form.is_valid():
-            patient_nam = app_form.cleaned_data["patient_name"]
+            patient_name = app_form.cleaned_data["patient_name"]
             treatment_type = app_form.cleaned_data["treatment_type"]
-            total_amount = request.POST['total_amount']
+            # total_amount = request.POST['total_amount']
             notes = request.POST['notes']
             date = datetime.strptime(request.POST['appointment'], "%m/%d/%Y %H:%M")
 
+            # Save the treatment and type of medication as well as the cost of medication 
+            treatment = Treatment(p_name=patient_name, treatments=treatment_type,notes=notes)
+            if treatment:
+                treatment.save()
+            else:
+                return HttpResponse({'msg': "no treatment was added"})
+            #extract the person whose its name is /patient_name/
+
+
+            patient_id = treatment.p_name.id
+            patient = Patients.objects.get(pk=patient_id)
+            # extract the amount of treatment and add it to the remaining amount of the patient
+            cost = treatment.treatments.total_cost
+            patient.remaining_amount += int(cost)
+            patient.save()
+            #print(patient.name)
+        
             
-            treatment = Treatment(p_name=patient_nam, treatments=treatment_type, total_cost=total_amount, notes=notes)
-            treatment.save()
-            
-            ne_app = Next_appointment(patient_name=patient_nam, treatment=treatment, date=date, notes=notes)
-            ne_app.save()
+            # now, we can make a new appointment for this particular patient
+            new_appointment = Next_appointment(patient_name=patient_name, treatment=treatment, date=date, notes=notes)
+            new_appointment.save()
             #ne_app.save()
-            return render(request, "dentist/appointments.html")
+            return HttpResponseRedirect(reverse('next_appointment'))
     
     else:
         return render(request, "dentist/appointments.html",{
             "form": Appointment()
         })
+
 #Save appointment and total cost
 def add_treatment(request, patient_id):
-    patient = Patients.objects.get(pk=patient_id)
-    # medication list 
-    mediction_list = Medication_list.objects.all()
-    if request.method == "POST":
-        #procedure is saved in Treatment model
-        procedure = request.POST['procedure']
-        #Date id saved in pre-appointment model
-        date = datetime.strptime(request.POST['date'], "%m/%d/%Y %H:%M")
-        #total_cost is saved in Treatment model
-        total_cost = int(request.POST["total_cost"])
-        #Paid is saved in Pre-app model
-        paid_amount = int(request.POST["paid_amount"])
-        #note is saved in all model except Patient model
-        notes = request.POST['notes']
-        #remaining_amount is saved in patient model
-        remaining_amount = int(total_cost - paid_amount)
-        patient.remaining_amount = remaining_amount
-        #Treatment model 
-        new_treatment = Treatment(p_name=patient, treatments=procedure, total_cost=total_cost, notes=notes)
-        if new_treatment is not None:
-            new_treatment.save()
-            patient.save()
-            #saving pre-appointment
-            pre_appointment = Previous_appointment(patient_name=patient, treatment=new_treatment, date=date, notes=notes, paid_amount=paid_amount)
-            if pre_appointment is not None:
-                pre_appointment.save()
+    return
+    # patient = Patients.objects.get(pk=patient_id)
+    # # medication list 
+    # mediction_list = Medication_list.objects.all()
+    # if request.method == "POST":
+    #     #procedure is saved in Treatment model
+    #     procedure = request.POST['procedure']
+    #     #Date id saved in pre-appointment model
+    #     date = datetime.strptime(request.POST['date'], "%m/%d/%Y %H:%M")
+    #     #total_cost is saved in Treatment model
+    #     total_cost = int(request.POST["total_cost"])
+    #     #Paid is saved in Pre-app model
+    #     paid_amount = int(request.POST["paid_amount"])
+    #     #note is saved in all model except Patient model
+    #     notes = request.POST['notes']
+    #     #remaining_amount is saved in patient model
+    #     remaining_amount = int(total_cost - paid_amount)
+    #     patient.remaining_amount = remaining_amount
+    #     #Treatment model 
+    #     new_treatment = Treatment(p_name=patient, treatments=procedure, total_cost=total_cost, notes=notes)
+    #     if new_treatment is not None:
+    #         new_treatment.save()
+    #         patient.save()
+    #         #saving pre-appointment
+    #         pre_appointment = Previous_appointment(patient_name=patient, treatment=new_treatment, date=date, notes=notes, paid_amount=paid_amount)
+    #         if pre_appointment is not None:
+    #             pre_appointment.save()
 
-                return HttpResponseRedirect(reverse('patient', args=(patient_id,)))
-        else:
-            return HttpResponse('this treatment cannot be saved!') 
+    #             return HttpResponseRedirect(reverse('patient', args=(patient_id,)))
+    #     else:
+    #         return HttpResponse('this treatment cannot be saved!') 
 
-
-def next_appointment(request):
-    next_appointments = Next_appointment.objects.all().order_by('-date')
-    if next_appointments.exists():
-        return render(request, "dentist/next_appointment.html",{
-            "next_appointment": next_appointments
+@login_required
+def upcoming_appointments(request):
+    upcoming_appointments = Next_appointment.objects.all().order_by('date')
+    
+    
+    #medlist = Medication_list.objects.all()
+    if upcoming_appointments.exists():
+        return render(request, "dentist/upcoming_appointments.html",{
+            "upcoming_appointments": upcoming_appointments,
+            #"medlist" : medlist
         })
     else:
     
-        return render(request, "dentist/next_appointment.html",{
+        return render(request, "dentist/upcoming_appointments.html",{
             "message" : "No upcoming appointments"
         }) 
     
-def modify_appointment(request, a_id):
-    edit_app = Next_appointment.objects.get(pk=a_id)
+# Change the currnet appointment    
+def change_appointment(request, a_id):
+    new_appointment = Next_appointment.objects.get(pk=a_id)
     patient_names = Patients.objects.all()
     treatments = Treatment.objects.all()
     
     
-    print(edit_app)
+    print(new_appointment)
     if request.method == 'POST':
         #name = request.POST['name']
         #treatment =request.POST['treatment']
@@ -178,22 +201,97 @@ def modify_appointment(request, a_id):
         notes = request.POST['notes']
 
         
-        #edit_app.name=name
-        #edit_app.treatment= treatment
-        edit_app.date = date
-        edit_app.notes = notes
-        edit_app.save()
+       
+        new_appointment.date = date
+        new_appointment.notes = notes
+        new_appointment.save()
         return HttpResponse('The appointment has been changed successfully')
     else:
-        return render(request, "dentist/modify_appointment.html",{
-            "edit_app" : edit_app,
+        return render(request, "dentist/change_appointment.html",{
+            "edit_app" : new_appointment,
 
             "patients" : patient_names,
             "treatments" : treatments
         })
 
-def archive(request, treatment_id):
-    treatment = Treatment.objects.get(pk=treatment_id)
+
+# the user can make a new appointment and 
+@csrf_exempt
+def update_schedule(request):
+    if request.method == "POST":
+        data =  json.loads(request.body)
+        id = data['id']
+        print(id)
+        name = data['name']
+        treatment = data['treatment']
+        date= data['date']
+        notes = data['notes']
+        paid_amount = data['paid_amount']
+        
+
+
+        #Delete this record from the Next appointment table
+        omitted_record = Next_appointment.objects.get(pk = id)
+        patient_id = omitted_record.patient_name.id 
+        patient = Patients.objects.get(pk=patient_id)
+        # archive this appointment
+        new_appointment = Next_appointment(patient_name=patient, treatment=treatment, date= date, notes=notes)
+        new_appointment.save()
+        # Archive the current appointment and save it 
+        archived_appointment = Previous_appointment(patient_name= omitted_record.patient_name, treatment=omitted_record.treatment, date= omitted_record.date, 
+        notes= omitted_record.notes, paid_amount=paid_amount)
+        archived_appointment.save()
+        # previous = Previous_appointment(patient_name= name, treatment= treatment, date=date, )
+        
+        patient.remaining_amount -= int(paid_amount)
+        patient.save()
+        print(f"id={patient_id}")
+        # print(omitted_record)
+        print(paid_amount)
+        # omitted_record.delete()
+        #archive this particular recodrd. Save it in the previous appointment
+        
+
+        return JsonResponse({"message": "successful"})
+    return JsonResponse({"message": "failed"})
+
+#when the user press archive button on the modal form
+@csrf_exempt
+def archived(request):
+    if request.method == "POST":
+        data= json.loads(request.body)
+        id = data['id']
+        paid_amount = data['paid_amount']
+        #select this record so that it will be archived to the previous appointment
+        record = Next_appointment.objects.get(pk=id)
+        #Archive this appointment
+        # get the patient from DB
+        patient_id = record.patient_name.id
+        patient = Patients.objects.get(pk=patient_id)
+        patient.remaining_amount -= int(paid_amount)
+        patient.save()
+        print(patient.remaining_amount)
+       
+        #  get the debit amount  and subtract paid_amount from the remaining amount
+        # patient.remaining_amount  -= int(paid_amount)
+        # patient.save()
+        
+        # print(debit_amount)
+        archived_record = Previous_appointment(patient_name=record.patient_name, treatment = record.treatment, date= record.date, notes= record.notes, paid_amount = paid_amount )
+        archived_record.save()
+        record.delete()
+        # print(record.treatment.total_cost)
+        
+        # record.save() 
+        # print(record.treatment.total_cost)
+
+        print(id)
+
+    
+        return JsonResponse({"message": "successful"})
+    return JsonResponse({"msg": "Failed"})
+def modal(request):
+    return render(request, "dentist/modal.html")
 
 
 
