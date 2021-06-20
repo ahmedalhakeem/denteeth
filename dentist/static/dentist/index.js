57337d90436fbc8f4589f831f1805fc0b5bb6603
ahmedalhakeem <ahmkam2011@gmail.com>
2021-06-20 14:12:18 +0300

    almost donee

--- dentist/static/dentist/index.js
+++ dentist/static/dentist/index.js
@@ -1,21 +1,29 @@
 document.addEventListener('DOMContentLoaded', function(){
    selectdate();
-//    document.querySelector('.appointment-form').style.display = 'none';
-//    document.querySelector('#add-appointment').addEventListener('click', ()=>{
-//        if(document.querySelector('.appointment-form').style.display === 'block'){
-//            document.querySelector('.appointment-form').style.display = 'none';
-//        }
-//         else{
-//             document.querySelector('.appointment-form').style.display = 'block';
+   
+    const add_med_list = document.querySelector('.add_med_list');
+    add_med_list.style.display = "none";
 
-//            }
-//        }
-//        );
-       
-}) 
+    const show_hide_btn = document.getElementById('add__treatbtn');
+    show_hide_btn.onclick = function(){
+    console.log("rer");
 
+     if(add_med_list.style.display === "none"){
+         add_med_list.style.display = "block";
+     }
+     else{
+         add_med_list.style.display = 'none';
+     }
+
+}   
+  
+});
 function selectdate(){
     $('#datepicker').datetimepicker()
 }
+
+
+
+ 
 
     
